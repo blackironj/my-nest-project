2c7a24246694d7cfd3b6d74f8222ceaab8bac9e8
hyunchul jung <blackironj@gmail.com>
2023-09-14 16:49:20 +0900

    Fixed. tsconfig

--- src/metadata.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {};
-    return { "@nestjs/swagger/plugin": { "models": [], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { type: String } } }]] } };
-};
